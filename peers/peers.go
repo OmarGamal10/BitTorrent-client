94796efcac76bb9e68bef0b542fd45cfe4a4e1b9
omargamal10 <ogamal1011@gmail.com>
2025-09-19 04:07:23 +0300

    chore: move peer type, logic and parsing to its own package

---
+++ peers/peers.go
@@ -0,0 +1,35 @@
+package peers
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
+type Peer struct {
+	IP   net.IP
+	Port uint16
+}
+
+// just for formatting
+func (p Peer) socketAddress() string {
+	return net.JoinHostPort(p.IP.String(), strconv.Itoa(int(p.Port)))
+}
+
+func UnMarshal(peerBuf []byte) ([]Peer, error) {
+	if len(peerBuf)%6 != 0 {
+		return nil, fmt.Errorf("corrupted peer list")
+	}
+	numPeers := len(peerBuf) / 6
+	peers := make([]Peer, numPeers)
+	for i := 0; i < numPeers; i++ {
+		ip := net.IPv4(peerBuf[i*6], peerBuf[i*6+1], peerBuf[i*6+2], peerBuf[i*6+3]) // idk if it even supports ipv6
+		port := uint16(peerBuf[i*6+4])<<8 | uint16(peerBuf[i*6+5])                   // big endian
+		peers[i] = Peer{
+			IP:   ip,
+			Port: port,
+		}
+		fmt.Println(peers[i].socketAddress())
+	}
+	return peers, nil
+}
--- torrentfile/tracker.go
+++ torrentfile/tracker.go
@@ -2,18 +2,13 @@ package torrentfile
 
 import (
 	"fmt"
-	"net"
 	"net/http"
 	"net/url"
 	"strconv"
 
 	"github.com/jackpal/bencode-go"
+	"github.com/omargamal10/BitTorrent-client/peers"
 )
-
-type Peer struct {
-	IP   net.IP
-	Port uint16 //normally listen on 6881, give up after 6889
-}
 
 // bencoded response from the tracker
 // for now I will focus on http trackers
@@ -22,29 +17,7 @@ type TrackerResponse struct {
 	Peers    string `bencode:"peers"`    //encoded as a list of 6 byte strings, 4 bytes IP, 2 bytes port Big Endian
 }
 
-// socket address of a peer
-func (p Peer) SocketAddress() string {
-	return net.JoinHostPort(p.IP.String(), strconv.Itoa(int(p.Port)))
-}
-
-func unMarshal(peerBuf []byte) ([]Peer, error) {
-	if len(peerBuf)%6 != 0 {
-		return nil, fmt.Errorf("corrupted peer list")
-	}
-	numPeers := len(peerBuf) / 6
-	peers := make([]Peer, numPeers)
-	for i := 0; i < numPeers; i++ {
-		ip := net.IPv4(peerBuf[i*6], peerBuf[i*6+1], peerBuf[i*6+2], peerBuf[i*6+3]) // idk if it even supports ipv6
-		port := uint16(peerBuf[i*6+4])<<8 | uint16(peerBuf[i*6+5])                   // big endian
-		peers[i] = Peer{
-			IP:   ip,
-			Port: port,
-		}
-	}
-	return peers, nil
-}
-
-func (t TorrentFile) GetTrackerResponse(peerID [20]byte, port uint16) ([]Peer, error) {
+func (t TorrentFile) GetTrackerResponse(peerID [20]byte, port uint16) ([]peers.Peer, error) {
 	baseUrl, err := url.Parse(t.Announce)
 	if err != nil {
 		return nil, err
@@ -52,7 +25,7 @@ func (t TorrentFile) GetTrackerResponse(peerID [20]byte, port uint16) ([]Peer, e
 
 	params := baseUrl.Query()
 	params.Set("info_hash", string(t.InfoHash[:]))
-	params.Set("peer_id", string(peerID[:]))
+	params.Set("peer_id", string(peerID[:])) // random
 	params.Set("port", strconv.Itoa(int(port)))
 	params.Set("uploaded", "0")
 	params.Set("downloaded", "0")
@@ -72,12 +45,9 @@ func (t TorrentFile) GetTrackerResponse(peerID [20]byte, port uint16) ([]Peer, e
 	if err != nil {
 		return nil, err
 	}
-	peers, err := unMarshal([]byte(trackerRes.Peers))
+	peers, err := peers.UnMarshal([]byte(trackerRes.Peers))
 	if err != nil {
 		return nil, err
-	}
-	for _, p := range peers {
-		fmt.Println(p.SocketAddress())
 	}
 	return peers, nil
 }
